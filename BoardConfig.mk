acf634a95541e2cf30368e898d55d26a822b6153
GuizaoBR <gui.germano.silva@gmail.com>
2016-12-10 01:42:06 -0200

    initial sources

---
+++ BoardConfig.mk
@@ -0,0 +1,164 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_BOARD_PLATFORM := mt6735m
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := Q5
+
+DEVICE_PATH := device/quantum/q5
+
+# Architecture
+#64 bit
+TARGET_ARCH := arm64
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := mt6753
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_CORTEX_A53 := true
+
+#32 bit
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_USERIMAGES_USE_EXT4:=true
+
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x40078000
+#Kenerl Source 
+TARGET_KERNEL_SOURCE := devoce/quantum/q5/kernel 
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CONFIG := q5_cyanogenmod13_debug_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+MTK_APPENDED_DTB_SUPPORT := yes
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+# Kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+BOARD_KERNEL_BASE = 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET = 0x03f88000
+BOARD_TAGS_OFFSET = 0x0df88000
+
+
+
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+
+#Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE:=2575302656
+BOARD_CACHEIMAGE_PARTITION_SIZE:=419430400
+BOARD_USERDATAIMAGE_PARTITION_SIZE:=12437684224
+
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+TARGET_INIT_VENDOR_LIB := libinit_q5
+
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Audio
+USE_CUSTOM_AUDIO_POLICY := 1
+BOARD_USES_MTK_AUDIO := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_HCILP_INCLUDED := 0
+BOARD_CONNECTIVITY_MODULE := conn_soc
+
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+		WITH_DEXPREOPT ?= true
+  endif
+endif
+
+# Display
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+MAX_VIRTUAL_DISPLAY_DIMENSION := 1
+PRESENT_TIME_OFFSET_FROM_VSYNC_NS := 0
+MTK_HWC_SUPPORT := yes
+MTK_HWC_VERSION := 1.4.1
+
+# Mediatek support
+BOARD_USES_MTK_HARDWARE := true
+
+# GPS
+ifeq ($(MTK_GPS_SUPPORT), yes)
+    BOARD_GPS_LIBRARIES := true
+else
+    BOARD_GPS_LIBRARIES := false
+endif
+
+#############################################################################################
+#Recovery
+#############################################################################################
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.mt6735
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/quantum/q5/ril
+
+# SELinux
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Wireless
+BOARD_WLAN_DEVICE := MediaTek
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM := /dev/wmtWifi
+WIFI_DRIVER_FW_PATH_AP := AP
+WIFI_DRIVER_FW_PATH_STA := STA
+WIFI_DRIVER_FW_PATH_P2P := P2P
+WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wmtWifi
+WIFI_DRIVER_STATE_ON := 1
+WIFI_DRIVER_STATE_OFF := 0
+
+# Misc
+EXTENDED_FONT_FOOTPRINT := true
+
+#############################################################################################
+#TWRP
+#############################################################################################
+#RECOVERY_VARIANT := twrp
+#TW_THEME := portrait_mdpi
+#TW_INCLUDE_CRYPTO := true
+#TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/fstab.twrp
+#TW_USE_KEY_CODE_TOUCH_SYNC := 330
+#RECOVERY_SDCARD_ON_DATA := true
+#
+#TARGET_USERIMAGES_USE_EXT4 := true
+#BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo aicp_q5-user
+add_lunch_combo aicp_q5-userdebug
+add_lunch_combo aicp_q5-eng
