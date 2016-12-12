4ade27952b823ef355cd1a0e97b16e847d9ff98c
GuizaoBR <gui.germano.silva@gmail.com>
2016-12-12 19:19:12 -0200

    Fixed kernel compile

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,10 +15,6 @@
 
 TARGET_BOARD_PLATFORM := mt6753
 
-
-
-
-
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := Q5
@@ -26,39 +22,25 @@ TARGET_BOOTLOADER_BOARD_NAME := Q5
 DEVICE_PATH := device/quantum/q5
 
 # Architecture
-#64 bit
+ifeq ($(FORCE_32_BIT),true)
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+else
 TARGET_ARCH := arm64
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := mt6753
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_CORTEX_A53 := true
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
 
-#32 bit
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-ARCH_ARM_HAVE_NEON := true
-ARCH_ARM_HAVE_VFP := true
-
-TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_USERIMAGES_USE_EXT4:=true
-
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x40078000
-#Kenerl Source 
-TARGET_KERNEL_SOURCE := device/quantum/q5/kernel 
-TARGET_KERNEL_CONFIG := q5_defconfig
-MTK_APPENDED_DTB_SUPPORT := yes
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+endif
 
 # Kernel
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
@@ -67,7 +49,16 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET = 0x03f88000
 BOARD_TAGS_OFFSET = 0x0df88000
 
-
+ifeq ($(FORCE_32_BIT),true)
+TARGET_KERNEL_ARCH := arm
+BOARD_KERNEL_CMDLINE = bootopt=64S3,32N2,32N2 androidboot.selinux=permissive
+BOARD_KERNEL_OFFSET = 0x00008000
+else
+TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_CMDLINE = bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_OFFSET = 0x00008000
+TARGET_USES_64_BIT_BINDER := true
+endif
 
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
 
